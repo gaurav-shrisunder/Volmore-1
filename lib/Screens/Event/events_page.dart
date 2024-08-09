1848b4e1e44b344c6c74f491815fdf6e66911719
kdThimble <kd@thimble.io>
2024-08-09 22:28:48 +0530

    merge changes

--- lib/Screens/Event/events_page.dart
+++ lib/Screens/Event/events_page.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
@@ -5,12 +8,15 @@ import 'package:volunterring/Screens/CreateLogScreen.dart';
 import 'package:volunterring/Screens/Event/events_widget.dart';
 import 'package:volunterring/Screens/Event/log_now_page.dart';
 import 'package:volunterring/Services/authentication.dart';
-import 'package:volunterring/Services/logService.dart';
 import 'package:volunterring/Utils/Colors.dart';
 import '../../Models/event_data_model.dart';
 
+enum SortOption { az, za, dateAsc, dateDesc }
+
 class EventPage extends StatefulWidget {
-  const EventPage({super.key});
+  final SortOption initialSortOption;
+
+  const EventPage({super.key, required this.initialSortOption});
 
   @override
   _EventPageState createState() => _EventPageState();
@@ -19,15 +25,21 @@ class EventPage extends StatefulWidget {
 class _EventPageState extends State<EventPage>
     with SingleTickerProviderStateMixin {
   final _authMethod = AuthMethod();
-  final _logMethods = LogServices();
   late Future<List<EventDataModel>> _eventsFuture;
   late TabController _tabController;
+
+  SortOption? _selectedOption;
+
+  List<EventListDataModel> mainEventList = [];
+
+  List<EventDataModel> sortedEventList = [];
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
-    _eventsFuture = _logMethods.fetchAllEventsWithLogs();
+    _eventsFuture = _authMethod.fetchEvents();
+    _selectedOption = widget.initialSortOption;
   }
 
   @override
@@ -63,44 +75,30 @@ class _EventPageState extends State<EventPage>
     return false;
   }
 
-  bool isAnyLogUnverified(List<dynamic> logs, DateTime date) {
-    for (var log in logs) {
-      DateTime logDate = (log['date'] as Timestamp).toDate();
-      if (logDate.year == date.year &&
-          logDate.month == date.month &&
-          logDate.day == date.day) {
-        if (!log['isSignatureVerified']) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  List<Map<String, dynamic>> getUpcomingEvents(List<EventDataModel> events) {
+  List<EventListDataModel> getUpcomingEvents(List<EventDataModel> events) {
     DateTime today = DateTime.now();
-    List<Map<String, dynamic>> upcomingEvents = [];
+    List<EventListDataModel> upcomingEvents = [];
     for (var event in events) {
       for (var dateMap in event.dates!) {
         Timestamp timestamp = dateMap['date'];
         DateTime date = timestamp.toDate();
         if (date.isAfter(today)) {
-          upcomingEvents.add({'event': event, 'date': date});
+          upcomingEvents.add(EventListDataModel(date: date, event: event));
         }
       }
     }
     return upcomingEvents;
   }
 
-  List<Map<String, dynamic>> getPastEvents(List<EventDataModel> events) {
+  List<EventListDataModel> getPastEvents(List<EventDataModel> events) {
     DateTime today = DateTime.now();
-    List<Map<String, dynamic>> pastEvents = [];
+    List<EventListDataModel> pastEvents = [];
     for (var event in events) {
       for (var dateMap in event.dates!) {
         Timestamp timestamp = dateMap['date'];
         DateTime date = timestamp.toDate();
         if (date.isBefore(today)) {
-          pastEvents.add({'event': event, 'date': date});
+          pastEvents.add(EventListDataModel(date: date, event: event));
         }
       }
     }
@@ -132,20 +130,39 @@ class _EventPageState extends State<EventPage>
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No events found'));
+            return Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFfa6513),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  textStyle: const TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                onPressed: () {
+                  Get.to(const CreateLogScreen());
+                },
+                child: const Text(
+                  'Create Event',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            );
           } else {
-            List<Map<String, dynamic>> todaysEvents = [];
-            List<Map<String, dynamic>> upcomingEvents =
+            List<EventListDataModel> todaysEvents = [];
+            List<EventListDataModel> upcomingEvents =
                 getUpcomingEvents(snapshot.data ?? []);
-            List<Map<String, dynamic>> pastEvents =
-                getPastEvents(snapshot.data!);
+            List<EventListDataModel> pastEvents =
+                getPastEvents(snapshot.data ?? []);
 
             for (var event in snapshot.data!) {
               if (containsToday(event.dates!)) {
-                todaysEvents.add({'event': event, 'date': DateTime.now()});
+                todaysEvents.add(
+                    EventListDataModel(event: event, date: DateTime.now()));
               }
             }
-
             return TabBarView(
               controller: _tabController,
               children: [
@@ -160,7 +177,7 @@ class _EventPageState extends State<EventPage>
     );
   }
 
-  Widget buildEventList(String title, List<Map<String, dynamic>> events) {
+  Widget buildEventList(String title, List<EventListDataModel> events) {
     return Column(
       children: [
         const SizedBox(height: 15),
@@ -177,26 +194,109 @@ class _EventPageState extends State<EventPage>
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              if (title == "Today's Events")
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFfa6513),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6),
+              Row(
+                children: [
+                  if (title == "Today's Events")
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFfa6513),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        textStyle:
+                            const TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Get.to(const CreateLogScreen());
+                      },
+                      child: const Text(
+                        'Create Event',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
                     ),
-                    textStyle:
-                        const TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                  onPressed: () {
-                    Get.to(const CreateLogScreen());
-                  },
-                  child: const Text(
-                    'Create Event',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                ),
+                  IconButton(
+                      onPressed: () {
+                        // showSortDialogBox(events);
+                        showDialog(
+                          context: context,
+                          builder: (_) {
+                            SortOption? selectedOption = _selectedOption;
+
+                            return StatefulBuilder(
+                              builder: (context, setState) {
+                                return SimpleDialog(
+                                  backgroundColor: Colors.white,
+                                  title: const Text("Sort by"),
+                                  children: [
+                                    Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        RadioListTile<SortOption>(
+                                          title: const Text('A-Z'),
+                                          value: SortOption.az,
+                                          groupValue: selectedOption,
+                                          onChanged: (SortOption? value) {
+                                            setState(() {
+                                              selectedOption = value;
+                                              //   events.sort((a, b) => a.event!.title!.compareTo(b.event!.title!));
+                                              //   _updateEventList(events); // Update the main event list
+                                            });
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                        RadioListTile<SortOption>(
+                                          title: const Text('Z-A'),
+                                          value: SortOption.za,
+                                          groupValue: selectedOption,
+                                          onChanged: (SortOption? value) {
+                                            setState(() {
+                                              selectedOption = value;
+                                              //   events.sort((a, b) => b.event!.title!.compareTo(a.event!.title!));
+                                              //    _updateEventList(events); // Update the main event list
+                                            });
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                        RadioListTile<SortOption>(
+                                          title: const Text('Date: Ascending'),
+                                          value: SortOption.dateAsc,
+                                          groupValue: selectedOption,
+                                          onChanged: (SortOption? value) {
+                                            setState(() {
+                                              selectedOption = value;
+                                              //    events.sort((a, b) => a.event!.date.compareTo(b.event!.date));
+                                              //   _updateEventList(events); // Update the main event list
+                                            });
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                        RadioListTile<SortOption>(
+                                          title: const Text('Date: Descending'),
+                                          value: SortOption.dateDesc,
+                                          groupValue: selectedOption,
+                                          onChanged: (SortOption? value) {
+                                            setState(() {
+                                              selectedOption = value;
+                                              //    events.sort((a, b) => b.event!.date!.compareTo(a.event!.date));
+                                              //   _updateEventList(events); // Update the main event list
+                                            });
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
+                      icon: const Icon(Icons.sort))
+                ],
+              ),
             ],
           ),
         ),
@@ -213,28 +313,12 @@ class _EventPageState extends State<EventPage>
                 child: ListView.builder(
                   itemCount: events.length,
                   itemBuilder: (context, index) {
-                    EventDataModel event = events[index]['event'];
-                    DateTime date = events[index]['date'];
-                    Color color = colorMap[event.groupColor] ?? Colors.pink;
-
-                    // Check if there is any unverified log for past events
-                    bool isPastEvent = title == "Past Events";
-                    bool isUpcomingEvent = title == "Upcoming Events";
-                    bool isTodayEvent = title == "Today's Events";
-                    bool isEnabled = containsToday(event.dates!) &&
+                    EventDataModel? event = events[index].event;
+                    DateTime date = events[index].date;
+                    Color color = colorMap[event?.groupColor] ?? Colors.pink;
+                    bool isEnabled = containsToday(event!.dates!) &&
                         title == "Today's Events";
-
-                    // Enable the button for past events if there are unverified logs
-                    if (isPastEvent) {
-                      isEnabled = !isAnyLogUnverified(event.logs as List, date);
-                    }
-
-                    // Disable the button for upcoming events
-                    if (isUpcomingEvent) {
-                      isEnabled = false;
-                    }
-
-                    String buttonText = isTodayEvent ? "Log Now" : "Verify";
+                    String buttonText = isEnabled ? "Log Now" : "Verify";
                     return EventWidget(
                       event,
                       color,
@@ -260,5 +344,98 @@ class _EventPageState extends State<EventPage>
               ),
       ],
     );
+  }
+
+  void showSortDialogBox(List<EventListDataModel> events) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        SortOption? selectedOption = _selectedOption;
+
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return SimpleDialog(
+              backgroundColor: Colors.white,
+              title: const Text("Sort by"),
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    RadioListTile<SortOption>(
+                      title: const Text('A-Z'),
+                      value: SortOption.az,
+                      groupValue: selectedOption,
+                      onChanged: (SortOption? value) {
+                        setState(() {
+                          selectedOption = value;
+                          //   events.sort((a, b) => a.event!.title!.compareTo(b.event!.title!));
+                          _updateEventList(
+                              events); // Update the main event list
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    RadioListTile<SortOption>(
+                      title: const Text('Z-A'),
+                      value: SortOption.za,
+                      groupValue: selectedOption,
+                      onChanged: (SortOption? value) {
+                        setState(() {
+                          selectedOption = value;
+                          //   events.sort((a, b) => b.event!.title!.compareTo(a.event!.title!));
+                          _updateEventList(
+                              events); // Update the main event list
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    RadioListTile<SortOption>(
+                      title: const Text('Date: Ascending'),
+                      value: SortOption.dateAsc,
+                      groupValue: selectedOption,
+                      onChanged: (SortOption? value) {
+                        setState(() {
+                          selectedOption = value;
+                          //    events.sort((a, b) => a.event!.date.compareTo(b.event!.date));
+                          _updateEventList(
+                              events); // Update the main event list
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    RadioListTile<SortOption>(
+                      title: const Text('Date: Descending'),
+                      value: SortOption.dateDesc,
+                      groupValue: selectedOption,
+                      onChanged: (SortOption? value) {
+                        setState(() {
+                          selectedOption = value;
+                          //    events.sort((a, b) => b.event!.date!.compareTo(a.event!.date));
+                          _updateEventList(
+                              events); // Update the main event list
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                )
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  _updateEventList(List<EventListDataModel> sortedEvents) {
+    for (var action in sortedEvents) {
+      log("Sorted List: ${jsonEncode(action.event?.date.toString())}");
+    }
+
+    setState(() {
+      // Replace the original list with the sorted one
+      mainEventList = List.from(sortedEvents);
+      //   sortedEventList = mainEventList;
+    });
   }
 }
