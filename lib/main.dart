f9d6232d838b1751434d0cd1f234d89c6e963e55
Gaurav's Macbook <user@gaurav-shrisunder.local>
2025-01-08 18:54:08 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+import 'dart:io';
+
+
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:get/get.dart';
+// import 'package:app_links/app_links.dart';
 import 'package:get/route_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -34,15 +40,20 @@ void main() async {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
+  static _MyAppState? of(BuildContext context) => context.findAncestorStateOfType<_MyAppState>();
+
 }
+
 
 class _MyAppState extends State<MyApp>  {
   bool isLoggedIn = false;
   final eventController = Get.find<EventController>();
+
+
 
   @override
   void initState() {
@@ -56,6 +67,137 @@ class _MyAppState extends State<MyApp>  {
     // clearPreferences();
     checkLocalStorage();
   }
+  String? screen;
+  Uri? _initialUri;
+  late AppLinks _appLinks;
+  StreamSubscription<Uri>? _linkSubscription;
+
+
+  Future<void> initDeepLinksIOS() async {
+    if (Platform.isIOS) {
+      //  await _getCurrentPosition();
+      _appLinks = AppLinks();
+      // Check initial link if app was in cold state (terminated)
+      /// Replaced the below::  final appLink = await _appLinks.getInitialAppLink();
+      final appLink = await _appLinks.getInitialLink();
+      if (appLink != null) {
+        print('getInitialAppLink: $appLink');
+        setState(() {
+          _initialUri = appLink;
+        });
+      }
+      // Handle link when app is in warm state (front or background)
+      _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
+        print('onAppLink: $uri');
+        _initialUri = uri;
+        screen = uri.queryParameters["artworktype"];
+        initDynamicLinksIOS();
+        // setState(() {
+        //
+        // });
+      });
+    }
+  }
+
+  Future<void> initDynamicLinksIOS() async {
+    // await _getCurrentPosition();
+
+    /*   setState(() {
+  locale = locale;
+  print('Inside::::::: $locale');
+});*/
+
+    print('Initial Url: ${_initialUri.toString()}');
+    if (_initialUri == null) {
+      setState(() {
+        screen = "splash";
+      });
+      // return "splash";
+    } else {
+  /*    print('ArtwornTye:; ${_initialUri?.queryParameters["artworkName"]}');
+      pdpParamRequest.sku = _initialUri?.queryParameters["artworkName"];
+      pdpParamRequest.isFromCollage = false;
+      pdpParamRequest.sku = _initialUri?.queryParameters["sku"];
+      pdpParamRequest.productUrl = _initialUri?.queryParameters["productUrl"];
+      pdpParamRequest.productId = _initialUri?.queryParameters["productId"];
+      pdpParamRequest.isSaleable =
+          _initialUri?.queryParameters["isSaleable"] == "true";
+      pdpParamRequest.artworkType = _initialUri?.queryParameters["artworktype"];
+      pdpParamRequest.isFromCustomLink = true;
+      navigatorKey.currentState
+          ?.pushNamed(splashScreen, arguments: pdpParamRequest);*/
+      /*if (pdpParamRequest.artworkType == "framerprod") {
+        screen = "pdp";
+        navigatorKey.currentState
+            ?.pushNamed(product_detail, arguments: pdpParamRequest);
+      } else if (pdpParamRequest.artworkType == "simple") {
+        screen = "painting_pdp";
+        navigatorKey.currentState
+            ?.pushNamed(product_painting_detail, arguments: pdpParamRequest);
+      }*/
+    }
+  }
+  Future<void> initDeepLinks() async {
+    if (Platform.isAndroid) {
+      //   await _getCurrentPosition();
+      _appLinks = AppLinks();
+      // Check initial link if app was in cold state (terminated)
+      final appLink = await _appLinks.getInitialLink();
+      if (appLink != null) {
+        print('getInitialAppLink: $appLink');
+        setState(() {
+          _initialUri = appLink;
+        });
+      }
+      // Handle link when app is in warm state (front or background)
+      _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
+        print('onAppLink: $uri');
+        setState(() {
+          _initialUri = uri;
+          screen = uri.queryParameters["artworktype"];
+        });
+      });
+    }
+  }
+
+  Future<String> initDynamicLinks() async {
+//    await _getCurrentPosition();
+    await initDeepLinks();
+
+
+    /*   setState(() {
+      locale = locale;
+      print('Inside::::::: $locale');
+    });*/
+
+    print('Initial Url: ${_initialUri.toString()}');
+    if (_initialUri == null) {
+      setState(() {
+        screen = "splash";
+      });
+      return "splash";
+    } else {
+     /* print('ArtwornTye:; ${_initialUri?.queryParameters["artworkName"]}');
+
+      pdpParamRequest.artistName = _initialUri?.queryParameters["artworkName"];
+      pdpParamRequest.sku = _initialUri?.queryParameters["sku"];
+      pdpParamRequest.productUrl = _initialUri?.queryParameters["productUrl"];
+      pdpParamRequest.productId = _initialUri?.queryParameters["productId"];
+      pdpParamRequest.isSaleable = _initialUri?.queryParameters["isSaleable"] as bool?;
+      pdpParamRequest.isFromCollage = false;
+      pdpParamRequest.artworkType = _initialUri?.queryParameters["artworktype"];*/
+      setState(() {
+        screen = _initialUri?.queryParameters["artworktype"] == "framerprod"
+            ? "pdp"
+            : "painting_pdp";
+      });
+      return _initialUri?.queryParameters["artworktype"] == "framerprod"
+          ? "pdp"
+          : "painting_pdp";
+    }
+  }
+
+
 
   @override
   void dispose() {
